50784e1abeb1649a3a9a0a8c37aae16c1e28a652
John Alade <john.alade@interswitchng.com>
2015-03-18 03:46:27 +0000

    Version number Config Change

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -20,8 +20,7 @@ grails.project.dependency.resolution = {
     dependencies {
 
         //runtime 'org.scribe:scribe:1.3.6'
-        runtime 'org.scribe:scribe:1.3.7.10'
-        compile 'com.auth0:java-jwt:2.0.1'
+        runtime 'org.scribe:scribe:1.3.7.10-SNAPSHOT'
 
         test    'org.gmock:gmock:0.8.2',
                 'org.objenesis:objenesis:1.2'
